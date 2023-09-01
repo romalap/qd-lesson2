5debb8d886861d5e141518b237dd22f26e511494
Roman Lapitski <roman.lapitski.contractor@draeger.com>
2023-09-01 16:27:22 +0300

    add argparser and tasks.json

--- main.py
+++ main.py
@@ -1,62 +1,83 @@
 # input is name of the function
+import argparse
 import os
-from pprint import pprint
-import re
+from random import choice
 from typing import List, Optional
+import ast
 
-FUNC = "read_file"
 DIR = r"C:\python_lesson\qd-lesson2"
+
+def parsed_args():
+    """ Parse args. """
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('-f', '--func', required=True, type=str, default=None, help='Please provide function name.')
+    return parser.parse_args()
 
 
 def scan_directory(directory: str, exclude: Optional[List[str]] = None) -> List[str]:
+    """ Find all python files. """
     files_list = []
     for root, _, files in os.walk(directory):
         found_exclude = False
-
         for exclude_dir in exclude:
             if exclude_dir in root:
                 found_exclude = True
                 break
-
         if found_exclude:
             continue
-
         for file in files:
             if file.endswith(".py"):
-                files_list.append(root + "\\" + file)
+                files_list.append(os.path.join(root, file))
     return files_list
 
 
-def read_file(filename: str) -> List[str]:
+def read_file(filename: str) -> str:
     """ Read file """
-    with open(filename, 'r', encoding="utf8") as f:
-        return f.readlines()
+    with open(filename, "r", encoding="utf8") as file:
+        return file.read()
 
 
-def find_func(modules: list) -> dict:
-    """ Search for all function occurrences in files  
+def find_func_def(modules: list) -> dict:
+    """ Search for all function occurrences in files.
     :param modules: list of paths to modules.
     """
     founded = {}
     for module in modules:
         content = read_file(module)
-        for line_number, line in enumerate(content):
-            if re.findall(f'{FUNC}(...)', line):
-            #if line.find(FUNC) != -1:
-                founded[line_number+1] = module
+        file_content = ast.parse(content)
+        for item in ast.walk(file_content):
+            if isinstance(item, ast.FunctionDef):
+                founded[item.name] = module
+    return founded
+
+
+def find_func_calls(modules: list) -> List[List[str]]:
+    """ Search for all calls occurrences in files.
+    :param modules: list of paths to modules.
+    """
+    founded = []
+    for module in modules:
+        content = read_file(module)
+        file_content = ast.parse(content)
+        for item in ast.walk(file_content):
+            if isinstance(item, ast.FunctionDef):
+                function_name = item.name
+                for item in ast.walk(item):
+                    if isinstance(item, ast.Call) and isinstance(item.func, ast.Name):
+                        list_ = [item.func.id, module, function_name]
+                        founded.extend([list_ for _ in range(len(list_))])
     return founded
 
 
 if __name__ == '__main__':
+    args = parsed_args()
     modules_list = scan_directory(
             directory=DIR,
             exclude=["venv", "tests"]
         )
+    found_def = find_func_def(modules_list)
+    found_calls = find_func_calls(modules_list)
+    print(found_calls)
 
-    found_list = find_func(modules_list)
-    for line_num, path in found_list.items():
-        print(f'File name: {path}. Line number: {line_num}')
-
-
-    # scan function should return not a filenames but an absolute paths
-    # create function that reads a file and check if function is defined in the file https://www.dataquest.io/blog/read-file-python/
+    # leson 3
+    # two separate functions to get func defenitions and func calls.
